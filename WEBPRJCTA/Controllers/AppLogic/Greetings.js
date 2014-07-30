00d2d70b341e1082f50ba25f737e89a6544a1bbb
ahmedrkhedr <ahmedramadan745i@gmail.com>
2014-07-30 23:51:18 +0300

    hi ahmeD

--- WEBPRJCTA/Controllers/AppLogic/Greetings.js
+++
@@ -1,9 +0,0 @@
-﻿var myApp = angular.module('myApp', []);
-myApp.factory('Data',function(){
-    return {text: "Integral Business Solutions"}
-})
-
-function HelloController($scope,Data) {
-    $scope.greeting = Data;
-}
-
